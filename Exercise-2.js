96c5bafd53007a6fd48ac6240636e917e67956c7
Aubin Ntwali <119161688+Ntwalii@users.noreply.github.com>
2024-08-23 14:00:43 +0200

    Removed unnecessary codes and updated the logic to accommodate different test cases.

--- Exercise-2.js
+++ Exercise-2.js
@@ -1,6 +1,6 @@
 const isPrimeNumber = (number) => {
-  for (let i = 2; i < Math.sqrt(number); i++) {
-      console.log(i)
+  if(number<1) return false
+  for (let i = 2; i <= Math.sqrt(number); i++) {
     if (number % i === 0) return false;
   }
   return true;
