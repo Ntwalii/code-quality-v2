c6dc92ec0de1d11476b46f6e787fc798d1682d55
Aubin Ntwali <119161688+Ntwalii@users.noreply.github.com>
2024-08-23 10:43:35 +0200

    Function to flatten an array

---
+++ Exercise-1.js
@@ -0,0 +1,6 @@
+const arrayDepth = (array) => 
+  Array.isArray(array) 
+    ? 1 + Math.max(0, ...array.map(element=>arrayDepth(element)))
+    : 0;
+
+const flattenArray=(array)=>array.flat(arrayDepth(array))
