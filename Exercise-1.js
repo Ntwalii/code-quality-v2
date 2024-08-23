0313742f29b94fed10cb5daf72a13815d6d55d8f
Aubin Ntwali <119161688+Ntwalii@users.noreply.github.com>
2024-08-23 13:53:59 +0200

    Updated codes with spaces for easy readability

--- Exercise-1.js
+++ Exercise-1.js
@@ -1,6 +1,6 @@
-const arrayDepth = (array) => 
-  Array.isArray(array) 
-    ? 1 + Math.max(0, ...array.map(element=>arrayDepth(element)))
+const arrayDepth = (array) =>
+  Array.isArray(array)
+    ? 1 + Math.max(0, ...array.map((element) => arrayDepth(element)))
     : 0;
 
-const flattenArray=(array)=>array.flat(arrayDepth(array))
+const flattenArray = (array) => array.flat(arrayDepth(array));
