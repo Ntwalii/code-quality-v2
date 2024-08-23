a1bfb4fcbc84238a61eb1a724b710986756b6b18
Aubin Ntwali <119161688+Ntwalii@users.noreply.github.com>
2024-08-23 14:03:43 +0200

    Update the logic and formatted the code

--- Exercise-2.js
+++ Exercise-2.js
@@ -1,5 +1,5 @@
 const isPrimeNumber = (number) => {
-  if(number<1) return false
+  if (number <= 1) return false;
   for (let i = 2; i <= Math.sqrt(number); i++) {
     if (number % i === 0) return false;
   }
